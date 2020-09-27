9369b0e670687d2c693b1fee9af07fd82310bced
HuanVH <38236556+kinghust69@users.noreply.github.com>
2020-09-28 00:24:36 +0700

    DB: Add database class and Type Data

---
+++ Altium Database Libraries/Lib/DataList.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Altium_Database_Libraries.Lib
+{
+    public class DataList
+    {
+        private string _PartType;
+        private string _PartNumber;
+        private string _Manufacturer;
+        private string _ManufacturerPartNumber;
+        private string _Description;
+        private string _LeadTime;
+        private string _Categories;
+        private string _Temperature;
+        private string _MountingType;
+        private string _Package;
+        private string _SupplierPackage;
+
+
+        public string PartType
+        {
+            get { return _PartType; }
+            set { _PartType = value; }
+        }
+
+        public string PartNumber
+        {
+            get { return _PartNumber; }
+            set { _PartNumber = value; }
+        }
+
+        public string Manufacturer
+        {
+            get { return _Manufacturer; }
+            set { _Manufacturer = value; }
+        }
+
+        public string ManufacturerPartNumber
+        {
+            get { return _ManufacturerPartNumber; }
+            set { _ManufacturerPartNumber = value; }
+        }
+
+        public string Description
+        {
+            get { return _Description; }
+            set { _Description = value; }
+        }
+
+
+        public string LeadTime
+        {
+            get { return _LeadTime; }
+            set { _LeadTime = value; }
+        }
+
+        public string Categories
+        {
+            get { return _Categories; }
+            set { _Categories = value; }
+        }
+
+        public string Temperature
+        {
+            get { return _Temperature; }
+            set { _Temperature = value; }
+        }
+
+        public string MountingType
+        {
+            get { return _MountingType; }
+            set { _MountingType = value; }
+        }
+
+        public string Package
+        {
+            get { return _Package; }
+            set { _Package = value; }
+        }
+
+        public string SupplierPackage
+        {
+            get { return _SupplierPackage; }
+            set { _SupplierPackage = value; }
+        }
+    }
+}
--- Altium Database Libraries/Lib/SQLData.cs
+++ Altium Database Libraries/Lib/SQLData.cs
@@ -2,15 +2,55 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.SQLite;
+using System.IO;
 
 namespace Altium_Database_Libraries.Lib
 {
     class SQLData
     {
 
-        public void SQLConnect(string database)
+        private SQLiteConnection connect = new SQLiteConnection();
+        private SQLiteCommand cmd;
+        private string dbName = string.Empty;
+        private void SQLConnect(string database)
         {
-
+            string strConnect = "Data Source=" + database + ";Version=3;";
+            connect.ConnectionString = strConnect;
+            connect.Open();
         }
+
+        private void SQLDisconnect()
+        {
+            connect.Close();
+        }
+
+        private void SQLCreate()
+        {
+            if (dbName == "") return;
+            string sql = "CREATE TABLE IF NOT EXISTS AltiumTable" +
+                " ([id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, Time nvarchar(50), IN_Va varchar(10) , IN_Vb varchar(10), IN_Vc varchar(10), V_DC varchar(10), Freq varchar(10), OUT_Va varchar(10), OUT_Vb varchar(10), " +
+                "OUT_Vc varchar(10), OUT_Ia varchar(10), OUT_Ib varchar(10), OUT_Ic varchar(10))";
+            if (!File.Exists(dbName))
+            {
+                SQLiteConnection.CreateFile(dbName);
+            }
+            SQLConnect(dbName);
+            cmd = new SQLiteCommand(sql, connect);
+            cmd.ExecuteNonQuery();
+            SQLDisconnect();
+        }
+
+        public void SQLAdd(List<DataList> list)
+        {
+            
+        }
+
+        public string Database
+        {
+            get { return dbName; }
+            set { dbName = value; }
+        }
+
     }
 }
